7700dc57aaa98774975825849c30201a60934ac9
tofan0412 <tofan12312@gmail.com>
2021-10-21 15:55:03 +0900

    백엔드, 프론트엔드 프로젝트 생성

---
+++ mokkozi/src/test/java/com/b303/mokkozi/MokkoziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b303.mokkozi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokkoziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mokkozi_fe/src/views/test/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    모꼬지
+    <div>
+      모꼬지2
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
