ad9c297c337012e8b36a5db7be9e3a5fde482e82
tofan0412 <tofan12312@gmail.com>
2021-11-14 04:40:53 +0900

    feat : [조웅현] 로그아웃 추가

--- mokkozi_fe/src/components/Foot.vue
+++ mokkozi_fe/src/components/Foot.vue
@@ -16,27 +16,40 @@
     </v-btn>
     </router-link>
 
-    <router-link to="/login" style="text-decoration: none; color:white">
-    <v-btn dark
-                dense
-                color="white"
-                text="text"
-                rounded="rounded"
-                class="my-2">Login
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <router-link v-if="this.$store.state.jwt === ''" to="/login" style="text-decoration: none; color:white">
+      <v-btn dark
+                  dense
+                  color="white"
+                  text="text"
+                  rounded="rounded"
+                  class="my-2">Login
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
     </router-link>
 
-    <router-link to="/login" style="text-decoration: none; color:white">
-    <v-btn dark
-                dense
-                color="white"
-                text="text"
-                rounded="rounded"
-                class="my-2">
-      <span>FAQ</span>
-      <v-icon>mdi-map-marker</v-icon>
-    </v-btn>
+    <router-link v-else to="" style="text-decoration: none; color:white">
+      <v-btn dark
+                  style="text-decoration: none; color:white"
+                  dense
+                  color="white"
+                  text="text"
+                  rounded="rounded"
+                  @click="logout()"
+                  class="my-2">Logout
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </router-link>
+
+    <router-link v-if="this.$store.state.jwt !== ''" :to="{ name: 'Profile', params: { userEmail: email }}" style="text-decoration: none; color:white">
+      <v-btn dark
+                  dense
+                  color="white"
+                  text="text"
+                  rounded="rounded"
+                  class="my-2">
+        <span>Mypage</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
     </router-link>
   </v-bottom-navigation>
 </template>
@@ -44,7 +57,18 @@
 export default {
   name: 'Foot',
   data: () => ({
-  })
+  }),
+  methods: {
+    logout() {
+      this.$store.dispatch("setJwt", '')
+    },
+  },
+  computed: {
+    email() {
+      const email = this.$store.state.user.email
+      return email
+    }
+  }
 }
 </script>
 <style scoped>
