1ba296071a9570bd52a55483545c0ed00b004991
정세원 <eeych@naver.com>
2021-11-15 19:21:03 +0900

    #S05P31B303-294 fix : 팔로우 목록 조회 시 스트림-리스트 매핑 오류 해결

--- mokkozi/src/main/java/com/b303/mokkozi/user/UserFollowRepository.java
+++ mokkozi/src/main/java/com/b303/mokkozi/user/UserFollowRepository.java
@@ -1,6 +1,8 @@
 package com.b303.mokkozi.user;
 
 import com.b303.mokkozi.entity.UserFollow;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +15,17 @@ public interface UserFollowRepository extends JpaRepository<UserFollow,Long> {
 
 //    List<UserFollow> getByUserFollower(Long id);
 
-    Optional<List<UserFollow>> findAllByToUserId(Long id);
+//    Optional<List<UserFollow>> findAllByToUserId(Long id);
 
     Stream<UserFollow> findAllByFromUserId(Long id);
+
+    int countByToUserId(Long id);
+
+    boolean existsByFromUserIdAndToUserId(Long fromUserId, Long toUserId);
+
+    Page<UserFollow> findByToUserId(Pageable pageable, Long id);
+
+    int countByFromUserId(Long id);
+
+    Page<UserFollow> findByFromUserId(Pageable pageable, Long id);
 }
