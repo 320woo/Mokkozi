7044433e165b31ede73d36f0b6f04d6db496d38d
320woo <320woo@naver.com>
2021-11-18 05:49:33 +0900

    feat : [임현우] 회전목마 작성중

--- mokkozi_fe/src/views/Home.vue
+++ mokkozi_fe/src/views/Home.vue
@@ -1,26 +1,12 @@
 <template>
   <v-container>
     <div class="mt-7">
-      <v-carousel
-        cycle
-        height="250"
-        width="700px;"
-        hide-delimiter-background
-        show-arrows-on-hover
-      >
-        <template v-slot:prev="{ on, attrs }">
-          <v-btn color="success" v-bind="attrs" v-on="on">Previous slide</v-btn>
-        </template>
-        <template v-slot:next="{ on, attrs }">
-          <v-btn color="info" v-bind="attrs" v-on="on">Next slide</v-btn>
-        </template>
-        <v-carousel-item v-for="(slide, i) in slides" :key="i">
-          <v-sheet :color="colors[i]" height="100%">
-            <v-row class="fill-height" align="center" justify="center">
-              <div class="text-h2"><img src="slide" /></div>
-            </v-row>
-          </v-sheet>
-        </v-carousel-item>
+      <v-carousel hide-delimiters>
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
       </v-carousel>
     </div>
     <div>
@@ -83,19 +69,22 @@ export default {
       defaultImage: defaultImage,
       recommends: [],
       address: [],
-      colors: [
-        "indigo",
-        "warning",
-        "pink darken-2",
-        "red lighten-1",
-        "deep-purple accent-4",
-      ],
-      slides: [
-        "../assets/images/데이트1.png",
-        "../assets/images/데이트2.png",
-        "../assets/images/데이트3.png",
-        "../assets/images/데이트4.png",
-        "../assets/images/데이트5.png",
+      items: [
+        {
+          src: "@/assets/images/커버.png",
+        },
+        {
+          src: "@/assets/images/데이트2.png",
+        },
+        {
+          src: "@/assets/images/데이트3.png",
+        },
+        {
+          src: "@/assets/images/데이트4.png",
+        },
+        {
+          src: "@/assets/images/데이트5.png",
+        },
       ],
     };
   },
