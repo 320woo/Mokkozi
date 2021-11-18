0394bb83038e0e0fe2b795afadb90c5a7325c540
정세원 <eeych@naver.com>
2021-11-19 05:21:37 +0900

    #S05P31B303-335 feat : Home 수정

--- mokkozi_fe/src/components/Top.vue
+++ mokkozi_fe/src/components/Top.vue
@@ -8,7 +8,7 @@
             text="text"
             rounded="rounded"
             class="my-2 btn">
-            <router-link to="/board" style="text-decoration: none; color:white">게시판</router-link></v-btn>
+            <router-link to="/board" style="text-decoration: none; color:white; font-size:18px;">게시판</router-link></v-btn>
             <v-btn
                 dark
                 dense
@@ -16,7 +16,7 @@
                 text="text"
                 rounded="rounded"
                 class="my-2 btn">
-            <router-link to="/meeting" style="text-decoration: none; color:white">랜선미팅</router-link></v-btn>
+            <router-link to="/meeting" style="text-decoration: none; color:white; font-size:18px;">랜선미팅</router-link></v-btn>
         </v-row>
     </v-footer>
 </template>
@@ -43,7 +43,7 @@ export default {
     position: fixed;
     left: auto;
     top: 0;
-    width: 700px; 
+    width: 700px;
     z-index: 10;
     }
 </style>
